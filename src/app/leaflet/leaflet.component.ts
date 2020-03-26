8afcd074c3add77c3c12e83752c6be7921c8f653
Alex <suarez9093@hotmail.com>
2020-03-25 19:35:08 -0600

    added a few map markers

--- src/app/leaflet/leaflet.component.ts
+++ src/app/leaflet/leaflet.component.ts
@@ -11,6 +11,18 @@ export class LeafletComponent implements OnInit {
 
   ngOnInit() {
     const mymap = L.map("mapid").setView([51.505, -0.09], 13);
+    var marker = L.marker([51.5, -0.09]).addTo(mymap);
+    var circle = L.circle([51.508, -0.11], {
+      color: "red",
+      fillColor: "#f03",
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(mymap);
+    var polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047]
+    ]).addTo(mymap);
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
       {
