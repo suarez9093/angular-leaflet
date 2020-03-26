b48765c0831b27f48d9846039fe0c4b71e927b5d
Alex <suarez9093@hotmail.com>
2020-03-25 19:14:59 -0600

    added header and got map to render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+// declare let L;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'angular-project';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    // const map = L.map("map").setView([51.505, -0.09], 13);
+    // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //   attribution:
+    //     '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
+  }
 }
---
+++ src/app/leaflet/leaflet.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+declare let L;
+
+@Component({
+  selector: "app-leaflet",
+  templateUrl: "./leaflet.component.html",
+  styleUrls: ["./leaflet.component.css"]
+})
+export class LeafletComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    const map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+}
