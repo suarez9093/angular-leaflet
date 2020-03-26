a299418bc91381099467a78b0752e41a091c28e9
Alex <suarez9093@hotmail.com>
2020-03-25 19:30:18 -0600

    figured out map selector was not selecting the div in the HTML

--- src/app/leaflet/leaflet.component.ts
+++ src/app/leaflet/leaflet.component.ts
@@ -10,11 +10,19 @@ export class LeafletComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const map = L.map("map").setView([51.505, -0.09], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    const mymap = L.map("mapid").setView([51.505, -0.09], 13);
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1Ijoic3VhcmV6OTA5MyIsImEiOiJjazdtbXk4Y2Uwa2FlM2xucjNocHdwOTR6In0.Idg85NpqSu9BGnw9FktCIQ"
+      }
+    ).addTo(mymap);
   }
 }
